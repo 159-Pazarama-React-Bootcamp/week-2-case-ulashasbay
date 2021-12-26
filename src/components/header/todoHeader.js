bf77ce34861201100be1100f357629fa75099a5c
Ulaş Hasbay <ulashasbay@gmail.com>
2021-12-26 23:13:40 +0300

    Odev-2 React Todo List

---
+++ src/components/header/todoHeader.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function TodoHeader({ username, theme, setTheme }) {
+  // Theme buttonuna basıldığında çalışacak fonksiyon
+  const HandleModeButton = () => {
+    if (theme === "dark") {
+      localStorage.setItem("theme", "light");
+      setTheme(localStorage.getItem("theme"));
+    } else {
+      localStorage.setItem("theme", "dark");
+      setTheme(localStorage.getItem("theme"));
+    }
+  };
+
+  return (
+    <div>
+      <h4 className="username">
+        Welcome <span id="userName">{username}!</span>
+      </h4>
+
+      {theme === "dark" ? (
+        <button
+          className="darkButton"
+          data-theme={theme}
+          onClick={HandleModeButton}
+        >
+          <i className="fas fa-sun"></i> Light Mode
+        </button>
+      ) : (
+        <button
+          className="darkButton"
+          data-theme={theme}
+          onClick={HandleModeButton}
+        >
+          <i className="fas fa-moon"></i> Dark Mode
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default TodoHeader;
